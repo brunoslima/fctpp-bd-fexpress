59c2c0641c1bdac4d38e47d00f94d68930a99415
Bruno Santos de Lima <brunoslima4@gmail.com>
2016-05-31 12:13:13 -0300

    Inicio do Layout do protótipo
    Arquivos iniciais relacionados ao layout do protótipo

---
+++ FoodExpress/system/view/login.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8"/>
+
+	<title>Tela de Login</title>
+
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+
+<body>
+
+	<div id="caixa-login">
+		
+		<form class="login">
+    		
+    		<p class="title">Tela de login</p>
+    		
+    		<input type="text" placeholder="Username" autofocus/>
+    		<i class="fa fa-user"></i>
+    		
+    		<input type="password" placeholder="Password" />
+    		<i class="fa fa-key"></i>
+    		
+    		<a href="#">Forgot your password?</a>
+    		
+    		<button>
+      			<i class="spinner"></i>
+      		<span class="state">Login</span>
+    </button>
+  </form>
+		
+
+		<!--<div id="conteudo-caixa-login">
+			
+			<form>
+
+				<h1>Tela de Login</h1>
+
+				<p>
+					<label for="username" class="unome" data-icon="u">Seu e-mail</label>
+					<input id="username" name="username" required="required" type="text" placeholder="login@mail.com"/>
+				</p>
+
+				<p>
+					<label for="password" class="suaSenha" data-icon="p">Sua senha:</label>
+					<input id="password" name="password" required="required" type="password" placeholder="sua senha">
+				</p>
+
+				<p class="login button"> 
+                    <input type="submit" value="Login" /> 
+				</p>
+			</form>-->
+
+		</div>
+
+	</div>
+
+</body>
+
+</html>
